3bb89ac6a0eb94e2519a225ebba7cb2728ac4870
Prashant Soni <prashantsoni@Prashants-MacBook-Air-5.local>
2026-04-27 15:32:11 +0530

    updated authcontroller authroute and minor change in utility

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,16 +1,16 @@
 const transporter = require('../config/mailer');
 
 const sendEmail = async ({ to, subject, html }) => {
-    try {
-        await transporter.sendMail({
-            from: `"SplitKar" <${process.env.EMAIL_USER}>`,
-            to,
-            subject,
-            html
-        });
-    } catch (err) {
-        console.error('Email send error:', err.message);
-    }
+  try {
+    await transporter.sendMail({
+      from: `"SplitKar" <${process.env.EMAIL_USER}>`,
+      to,
+      subject,
+      html
+    });
+  } catch (err) {
+    console.error('Email send error:', err.message);
+  }
 };
 
 module.exports = sendEmail;
